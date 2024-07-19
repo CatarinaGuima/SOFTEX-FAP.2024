d76ce52e04dc1e42cffaa449c5467ca6d8b75c39
Catarina Guimaraes <catarina.guimaraes15@gmail.com>
2024-07-18 23:08:27 -0300

    Eighth Commit

--- Atividade 04 -Condicionais e controle de fluxo/index.js
+++ Atividade 04 -Condicionais e controle de fluxo/index.js
@@ -55,8 +55,8 @@
 // }
 // //Questão 06
 // // Crie um programa que receba o nome de duas pessoas e exiba qual delas possui o maior número de caracteres em seu nome.
-// const nome1 = prompt("Digite o primeiro nome: "); 
-// const nome2 = prompt("Digite o segundo nome: "); 
+// const nome1 = prompt("Digite o primeiro nome: ");
+// const nome2 = prompt("Digite o segundo nome: ");
 // if (nome1 === null || nome2 === null){
 //   console.log("Você não digitou nenhum nome.");
 // } else if (nome1.length > nome2.length) {
@@ -64,4 +64,98 @@
 // } else {
 //   console.log(`O nome ${nome2} possui o maior número de caracteres do que ${nome1}`);
 // }
-//Questão 07
+// // Questão 07
+// // Desenvolva um programa que leia um caractere e verifique se ele é uma vogal ou uma consoante.
+// const caractere = prompt("Escolha 01(UM) caractere: ");
+// if (caractere === null) {
+//   console.log("A entrada foi cancelada.");
+// } else if (caractere.length !== 1) {
+//   console.log("Digite apenas um caractere.");
+// } else if (caractere.toUpperCase() === "A" || caractere.toUpperCase() === "E" || caractere.toUpperCase() === "I" || caractere.toUpperCase() === "O" || caractere.toUpperCase() === "U") {
+//   console.log(`O caractere: '${caractere}' é uma vogal`);
+// } else {
+//   console.log(`O caractere: '${caractere}' é uma consoante`);
+// }
+// //Questão 08
+// // Faça um programa que receba três números e os imprima em ordem crescente.
+// const numeros: number [] = []
+// for (let i = 1 ; i <= 3 ; i++){
+//     const numero = Number(prompt(`Entre com o ${i}º número inteiro: `));
+//     numeros.push(numero);
+// }
+// numeros.sort((a, b) => a - b);
+// console.log(numeros);
+// //Questão 09
+// // Elabore um programa que calcule o IMC (Índice de Massa Corporal) de uma pessoa, dado o peso e a altura. Em seguida, exiba se a pessoa está abaixo do peso, com peso normal, com sobrepeso, obesa ou muito obesa.
+// const altura = Number(prompt("Por favor, informe a altura: "));
+// const peso = Number(prompt("Agora, informe o peso: "));
+// const IMC = peso / altura ** 2;
+// if (IMC > 0 && IMC <= 18.5) {
+//     console.log(`O IMC: ${IMC.toFixed(2)}. Você está abaixo do peso!`);
+// } else if (IMC > 18.5 && IMC <= 24.9) {
+//     console.log(`O IMC: ${IMC.toFixed(2)}. Você está com peso normal!`);
+// } else if (IMC >= 25.0 && IMC < 29.9) {
+//     console.log(`O IMC: ${IMC.toFixed(2)}. Você está com sobrepeso!`);
+// } else if (IMC >= 30.0 && IMC < 39.9) {
+//     console.log(`O IMC: ${IMC.toFixed(2)}. Você está com obesidade!`);
+// } else if (IMC >= 40.0) {
+//     console.log(`O IMC: ${IMC.toFixed(2)}. Você está muito obesa!`);
+// }
+// //Questão 10
+// // Escreva um programa que receba um número de mês (1 a 12) e exiba o nome do mês correspondente.
+// const mes = parseInt(prompt("Escolha um número de 1 à 12: ")!);
+// switch (mes) {
+//   case 1:
+//     console.log("Bem-vindo(a) ao mês de Janeiro");
+//     break;
+//   case 2:
+//     console.log("Bem-vindo(a) ao mês de Fevereiro");
+//     break;
+//   case 3:
+//     console.log("Bem-vindo(a) ao mês de Março");
+//     break;
+//   case 4:
+//     console.log("Bem-vindo(a) ao mês de Abril");
+//     break;
+//   case 5:
+//     console.log("Bem-vindo(a) ao mês de Maio");
+//     break;
+//   case 6:
+//     console.log("Bem-vindo(a) ao mês de Junho");
+//     break;
+//   case 7:
+//     console.log("Bem-vindo(a) ao mês de Julho");
+//     break;
+//   case 8:
+//     console.log("Bem-vindo(a) ao mês de Agosto");
+//     break;
+//   case 9:
+//     console.log("Bem-vindo(a) ao mês de Setembro");
+//     break;
+//   case 10:
+//     console.log("Bem-vindo(a) ao mês de Outubro");
+//     break;
+//   case 11:
+//     console.log("Bem-vindo(a) ao mês de Novembro");
+//     break;
+//   case 12:
+//     console.log("Bem-vindo(a) ao mês de Dezembro");
+//     break;
+//   default:
+//     console.log("Número inválido! Digite um número entre 1 e 12.");
+//     break;
+// }
+// //Questão 11
+// // Desenvolva um programa que leia o salário de um funcionário e calcule o valor do seu aumento. Para salários superiores a R$ 1.500,00, o aumento deve ser de 10%. Caso contrário, o aumento é de 15%.
+// const salario = parseFloat(prompt("Informe o salário R$: ")!);
+// if (salario >= 1.500) {
+//     const aumento = salario * 0.10;
+//     console.log(`O aumento é de R$ ${aumento.toFixed(2)}`);
+//     const novaSalario = salario + aumento;
+//     console.log(`O novo salário é R$ ${(novaSalario).toFixed(2)}`);
+// } else {
+//     const aumento = salario * 0.15;
+//     console.log(`O aumento é de R$ ${aumento.toFixed(2)}`);
+//     const novaSalario = salario + aumento;
+//     console.log(`O novo salário é R$ ${(novaSalario).toFixed(2)}`);
+// }
